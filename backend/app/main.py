8d3d23211787c3d25884931dc55153ccc3091c43
Andrei Meshchanchuk <a.meshchanchuk@skyeng.ru>
2024-05-22 17:40:05 +0200

    Implement summarize api method

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask import request
 from flask import jsonify
 from sentry_sdk.integrations.flask import FlaskIntegration
 import sentry_sdk
 from .summarizer import youtube_video
+import re
 
 
 def create_app(testing: bool = True):
@@ -22,14 +24,23 @@ def create_app(testing: bool = True):
 
     @app.route("/")
     def index():
-        return f"<p>Hello, World!</p>"
+        return f"<p>Index</p>"
 
-    @app.route("/api/summarize")
+    @app.route("/api/summarize", methods=['POST'])
     def summarize():
-        video_url = 'https://www.youtube.com/watch?v=-3toF-B2lEk'
+        if not request.json:
+            return jsonify({'error': 'Request must be JSON'}), 400
+
+        if 'youtube_video_url' not in request.json:
+            return jsonify({'error': 'Missing YouTube URL'}), 400
+
+        youtube_video_url = request.json.get('youtube_video_url')
+
+        if not re.match(r'(https?://)?(www\.)?youtube\.com/watch\?v=[a-zA-Z0-9_-]{11}', youtube_video_url):
+            return jsonify({'error': 'Invalid YouTube URL'}), 400
 
         data = {
-            'summary': youtube_video.summarize(video_url)
+            'summary': youtube_video.summarize(youtube_video_url)
         }
 
         return jsonify(data)
