1676751fee7f01b4f3279d82ccfb68c0af61e63b
Andrei Meshchanchuk <a.meshchanchuk@skyeng.ru>
2024-05-22 16:40:46 +0200

    Sentry

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
+import sentry_sdk
 
-load_dotenv()
 
 def create_app(testing: bool = True):
+    load_dotenv()
+
+    sentry_sdk.init(git
+        dsn=os.environ.get("SENTRY_DSN"),
+        enable_tracing=True,
+        integrations=[FlaskIntegration()],
+    )
+
     app = Flask(__name__)
 
     app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
@@ -12,5 +21,10 @@ def create_app(testing: bool = True):
     @app.route("/")
     def index():
         return f"<p>Hello, World!</p>"
+
+    @app.route("/api/summarize")
+    def summarize():
+        division_by_zero = 1 / 0
+        return f"<p>Summarize</p>"
 
     return app
