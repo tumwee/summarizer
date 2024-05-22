52bc8e6915a637896a6f6e925fc05b8c2e245930
Andrei Meshchanchuk <a.meshchanchuk@skyeng.ru>
2024-05-22 17:22:43 +0200

    Implement AI logic on backend and try to deploy to GCP

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,16 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask import jsonify
 from sentry_sdk.integrations.flask import FlaskIntegration
 import sentry_sdk
+from .summarizer import youtube_video
 
 
 def create_app(testing: bool = True):
     load_dotenv()
 
-    sentry_sdk.init(git
+    sentry_sdk.init(
         dsn=os.environ.get("SENTRY_DSN"),
         enable_tracing=True,
         integrations=[FlaskIntegration()],
@@ -24,7 +26,12 @@ def create_app(testing: bool = True):
 
     @app.route("/api/summarize")
     def summarize():
-        division_by_zero = 1 / 0
-        return f"<p>Summarize</p>"
+        video_url = 'https://www.youtube.com/watch?v=-3toF-B2lEk'
+
+        data = {
+            'summary': youtube_video.summarize(video_url)
+        }
+
+        return jsonify(data)
 
     return app
---
+++ backend/app/summarizer/youtube_video.py
@@ -0,0 +1,57 @@
+import os
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers.audio import OpenAIWhisperParser
+from langchain_community.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+from langchain_together import Together
+from langchain.prompts import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+
+
+def summarize(url):
+    save_dir = "docs/youtube"
+    loader = GenericLoader(
+        YoutubeAudioLoader([url], save_dir),
+        OpenAIWhisperParser()
+    )
+    docs = loader.load()
+    remove_video_files(docs)
+
+    prompt = PromptTemplate(
+        template="""Summarize the main points and their comprehensive explanations from below text, presenting them under appropriate headings. Use various Emoji to symbolize different sections, and format the content as a cohesive paragraph under each heading. Ensure the summary is clear, detailed, and informative, reflecting the executive summary style found in news articles. Avoid using phrases that directly reference 'the script provides' to maintain a direct and objective tone. \
+        ```{text}```
+        """,
+        input_variables=["text"]
+    )
+
+    combine_prompt = PromptTemplate(
+        template="""Summarize the main points and their comprehensive explanations from the provided text, organizing them under appropriate headings and using various Emoji to symbolize different sections. Format the content as a cohesive paragraph under each heading, ensuring clarity, detail, and informativeness akin to the executive summary style found in news articles. Maintain a direct and objective tone without directly referencing 'the script provides'. \
+        ```{text}```
+        """,
+        input_variables=["text"]
+    )
+
+    together_llm = Together(
+        model="meta-llama/Llama-3-70b-chat-hf",
+        temperature=0,
+        max_tokens=128,
+        top_k=1,
+        together_api_key=os.environ.get("TOGETHER_API_KEY")
+    )
+
+    summarize_chain = load_summarize_chain(
+        together_llm,
+        chain_type="map_reduce",
+        map_prompt=prompt,
+        combine_prompt=combine_prompt
+    )
+
+    result = summarize_chain.invoke(docs)
+
+    return result["output_text"]
+
+
+
+def remove_video_files(docs):
+    for doc in docs:
+        if os.path.exists(doc.metadata['source']):
+            os.remove(doc.metadata['source'])
